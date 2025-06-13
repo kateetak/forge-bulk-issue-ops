6c5dfb526f71cfcd9043c1981546e24af465ab89
Dugald Morrow <dmorrow@atlassian.com>
2025-06-14 09:57:11 +1000

    Added dark mode support

--- static/spa/src/index.tsx
+++ static/spa/src/index.tsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import '@atlaskit/css-reset';
 import './Main.css';
 import Main from './Main';
+import { view } from "@forge/bridge";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const renderApp = async (): Promise<void> => {
+  await view.theme.enable();
+  ReactDOM.render(
+    <React.StrictMode>
+      <Main />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+renderApp();
